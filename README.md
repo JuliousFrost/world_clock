8cd440f7269581d71f3bbb6d4f942a080874e6d6
Niloy Julious <niloyjulious@gmail.com>
2021-06-14 16:17:46 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# world_clock
+ Flutter App to view Times of different cities
---
+++ lib/services/world_time.dart
@@ -0,0 +1,40 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+import 'package:intl/intl.dart';
+
+class WorldTime {
+
+  String location;
+  late String time;
+  String flag;
+  String url;
+  bool? isDaytime;
+
+  WorldTime({ required this.location, required this.flag, required this.url});
+
+  Future<void> getTime() async {
+    try{
+      Uri apiUrl = Uri.parse('http://worldtimeapi.org/api/timezone/$url');
+      Response response = await get(apiUrl);
+      Map data = jsonDecode(response.body);
+      // print(data);
+      String datetime = data['datetime'];
+      String offset = data['utc_offset'].substring(0,3);
+      // print(datetime);
+      // print(offset);
+      DateTime now = DateTime.parse(datetime);
+      now = now.add(Duration(hours: int.parse(offset)));
+      print(now);
+
+      isDaytime = now.hour > 6 && now.hour <19 ? true:false;
+      time = DateFormat.jm().format(now);
+
+    }
+    catch(e){
+      print('caught error : $e');
+      time = 'Could not get time data';
+
+    }
+  }
+
+}
